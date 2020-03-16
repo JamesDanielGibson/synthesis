0c8e811ddd426c15d2389fa19ca97598a87ed34f
JamesDanielGibson <g18g0723@campus.ru.ac.za>
2020-03-16 14:30:49 +0200

    submit

--- Synthesis/Library.fs
+++ Synthesis/Library.fs
@@ -53,13 +53,35 @@ let digits a =
     inner a
 
 let minmax a =
+    failwith "not implemented"
      
 
-let isLeap _ =
-    failwith "Not implemented"
+let isLeap a = 
+    match a%1 with
+    |0->match a%4 with
+       |0 -> match a%100 with
+           |0 -> match a%400 with
+               |0-> true
+               |_->false
+           |_->true
+       |_-> false
+    |_ ->failwith ""
 
-let month _ =
-    failwith "Not implemented"
+let month a =
+    match a with
+       |1 -> ("January",31)
+       |2 -> ("February",28)
+       |3 -> ("March",31)
+       |4 -> ("April",30)
+       |5 -> ("May",31)
+       |6 -> ("June",30)
+       |7 -> ("July",31)
+       |8 -> ("August",31)
+       |9 -> ("September",30)
+       |10 -> ("October",31)
+       |11 -> ("November",30)
+       |12 -> ("December",31)
+       |_ -> failwith "not a month"
 
 let toBinary _ =
     failwith "Not implemented"
